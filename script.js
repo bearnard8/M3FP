b364774339c6954d75950b3858836c37bdf9a7dd
Stefano Bernardo <stefano.berna8@gmail.com>
2024-01-21 22:08:06 +0100

    Rev. 4

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
+/*aggiunta/rimozione div scrollando*/
+
 let addedDiv = document.getElementsByClassName("disappear");
 
 window.addEventListener("scroll", function() {
-    addDiv();
-    removeDiv();
+    addRemoveDiv();
 })
 
-function addDiv() {
+function addRemoveDiv() {
     let y = window.scrollY;
-    console.log(y);
     let i = 0;
     if (y>200 && y<400) {
         addedDiv[i].style.display = "block";
@@ -15,12 +15,6 @@ function addDiv() {
         i += 1;
         addedDiv[i].style.display = "block";
     }
-}
-
-function removeDiv() {
-    let y = window.scrollY;
-    console.log(y);
-    let i = 0;
     if (y < 200) {
         addedDiv[i].style.display = "none";
     } else if (y < 400) {
@@ -28,3 +22,64 @@ function removeDiv() {
         addedDiv[i].style.display = "none";
     }
 }
+
+/*carosello*/
+
+// let items = document.querySelectorAll('.carousel .carousel-item')
+// console.log(items);
+
+// items.forEach((el) => {
+//     const minPerSlide = 6;
+//     let next = el.nextElementSibling;
+//     for (let i=1; i<minPerSlide; i++) {
+//         if (!next) {
+//         	next = items[0];
+//       	}
+//         let cloneChild = next.cloneNode(true);
+//         el.appendChild(cloneChild.children[0]);
+//         next = next.nextElementSibling;
+//     }
+// })
+
+const carouselIds = ['#filmCarousel1', '#filmCarousel2', '#filmCarousel3', '#filmCarousel4', '#filmCarousel5'];
+let screenWidth = window.innerWidth;
+
+carouselIds.forEach((carouselId) => {
+    let carousel = document.querySelector(carouselId);
+    
+    if (carousel) {
+        let items = carousel.querySelectorAll('.carousel-item');
+
+        if (screenWidth >= 768) {
+            items.forEach((el) => {
+                const minPerSlide = 6;
+                let next = el.nextElementSibling;
+    
+                for (let i = 1; i < minPerSlide; i++) {
+                    if (!next) {
+                        next = items[0];
+                    }
+    
+                    let cloneChild = next.cloneNode(true);
+                    el.appendChild(cloneChild.children[0]);
+                    next = next.nextElementSibling;
+                }
+            });
+        } else {
+            items.forEach((el) => {
+                const minPerSlide = 2;
+                let next = el.nextElementSibling;
+    
+                for (let i = 1; i < minPerSlide; i++) {
+                    if (!next) {
+                        next = items[0];
+                    }
+    
+                    let cloneChild = next.cloneNode(true);
+                    el.appendChild(cloneChild.children[0]);
+                    next = next.nextElementSibling;
+                }
+            });
+        }
+    }
+});
